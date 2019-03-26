5206fbb25e4698390594585c452a29e0dc520fc4
tbrou <tbrou@PCP-066.natsystem.fr>
2019-03-26 20:26:42 +0100

    * postman + jax-rs annotations improvements

--- quiz-rest-services/src/main/java/fr/epita/quiz/resources/QuestionResource.java
+++ quiz-rest-services/src/main/java/fr/epita/quiz/resources/QuestionResource.java
@@ -1,11 +1,13 @@
 package fr.epita.quiz.resources;
 
-import java.util.ArrayList;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -13,23 +15,35 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import fr.epita.quiz.datamodel.Question;
+import fr.epita.quiz.services.dataaccess.QuestionJPADAO;
 
 @Path("/questions")
 public class QuestionResource {
 
+	@Inject
+	QuestionJPADAO questionDAO;
+	
 	
 	@GET
 	@Path("/")
 	@Produces(value = { MediaType.APPLICATION_JSON })
 	public Response findAllQuestions(@QueryParam("s") String inputString) {
+		Question criteria = new Question();
+		criteria.setContent(inputString);
+		List<Question> searchResults = questionDAO.search(criteria);
 		
-		List<Question> messages = new ArrayList<Question>();
-		Question question = new Question();
-		question.setContent("What is Java?");
-		messages.add(question);
-		
-		return Response.ok(messages).build();
+		return Response.ok(searchResults).build();
 	}
+	
+	@POST
+	@Path("/")
+	@Consumes(value = { MediaType.APPLICATION_JSON })
+	public Response createQuestion(Question question) throws URISyntaxException {
+		questionDAO.create(question);
+		return Response.created(URI.create("questions/" + String.valueOf(question.getId()))).build();
+		
+	}
+	
 	
 	
 }
